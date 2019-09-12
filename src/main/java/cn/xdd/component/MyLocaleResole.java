f9ecb46346f19e5ea43c4e48da944bd408961b43
XDD <704566072@qq.com>
2019-09-13 00:34:34 +0800

    add login pages inernationalizaion

---
+++ src/main/java/cn/xdd/component/MyLocaleResole.java
@@ -0,0 +1,41 @@
+package cn.xdd.component;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ * @author xchb
+ * @version 1.0
+ * @ClassName MyLocationResolve
+ * @description 自定义区域信息解析器
+ * @createTime 2019年09月13日 00:05
+ */
+public class MyLocaleResole implements LocaleResolver {
+
+    @Override
+    public Locale resolveLocale(HttpServletRequest request) {
+
+        //获取请求中所带的语言地区信息
+        String language_country=request.getParameter("l");
+        Locale locale=null;
+        //请求中携带区域语言信息参数
+        if(!StringUtils.isEmpty(language_country)){
+            String[] str=language_country.split("_");
+            if(str.length>=2){
+                locale=new Locale(str[0],str[1]);
+            }
+        }else {  //请求中没有携带区域语言信息参数，使用请求头中的区域信息（根据浏览器英语来设置的）
+            locale=request.getLocale();
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+
+    }
+}
---
+++ src/main/java/cn/xdd/config/MyWebMVCConfig.java
@@ -0,0 +1,59 @@
+package cn.xdd.config;
+
+import cn.xdd.component.MyLocaleResole;
+import cn.xdd.intercept.OverallIntercept;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * @author xchb
+ * @version 1.0
+ * @ClassName InterceptConfig
+ * @description 自定义的springMvc配置
+ * @createTime 2019年09月11日 19:40
+ */
+
+//@EnableWebMvc此注解表示，全全面接管springMVC，
+//不使用springboot的所有springmvc配置，其中包含类路径下的static,public等等的文件映射
+
+@Configuration
+public class MyWebMVCConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加全局拦截器
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new OverallIntercept()).addPathPatterns("/*");
+    }
+
+
+    /**
+     * 添加视图控制器
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //请求/1路径时映射到index.html页面
+        registry.addViewController("/1").setViewName("index");
+    }
+
+
+    /**
+     * 向容器中添加自定义的区域信息解析器组件
+     * @return
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResole();
+    }
+}
